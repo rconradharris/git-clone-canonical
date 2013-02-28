be2b8909814ae1d09c19a00d050eed4c4dfcef67
Rick Harris <rconradharris@gmail.com>
2013-02-28 11:26:51 -0600

    Initial checkin

---
+++ README.rst
@@ -0,0 +1,26 @@
+===================
+git-clone-canonical
+===================
+
+git extension that lets you easily fetch the source code for popular
+open-source projects.
+
+Installation
+============
+
+::
+
+    pip install git-clone-canonical
+      - install binary
+      - clone repo to /usr/local/git-clone-canonical
+      - add any tab-completion hook
+
+
+Usage
+=====
+
+::
+
+    clone: git clone-canonical sqlalchemy
+    update: git clone-canonical --update
+    search: git clone-canonical --search sql
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+setuptools.setup(
+    name='git-clone-canonical',
+    version='0.0.1',
+    url='https://github.com/rconradharris/git-clone-canonical',
+    license='MIT',
+    author='Rick Harris',
+    author_email='rconradharris@gmail.com',
+    description='git clone from canonical location by name',
+    long_description=__doc__,
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[''],
+    scripts=['bin/git-clone-canonical'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
